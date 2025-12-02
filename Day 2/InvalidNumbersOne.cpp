91a85367602608ab2660fb06800285fba32a05b9
Alex Bradshaw <xander.bradshaw@gmail.com>
2025-12-02 20:15:29 +0800

    Ideas for day 2 part one
    Just wrote some ideas down to implement when I have time.

---
+++ Day 2/InvalidNumbersOne.cpp
@@ -0,0 +1,14 @@
+
+
+int main()
+{
+    //Read in the file and tokenize based on the commas, split each token on the hyphen and then you can increment between the two numbers.
+    //Split the number as a string and then check for equality.
+    //Increment through and see if the first half of a string equals the second half of a string.
+    //If it does then grab the number it is, if not then move to next in the range.
+    //If the length of the string is not divisible by 2, then it doesn't have an ID.
+
+
+
+    return 0;
+}
