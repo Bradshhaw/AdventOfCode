52e7ba6562424104868ad08a2f001e4152898bfc
Alex Bradshaw <xander.bradshaw@gmail.com>
2025-12-02 20:01:20 +0800

    Part 2 solve change
    Added a small change to the code to allow it to solve for part 2. Made another file just so easy visual of both are on there.

---
+++ Day 1/Day1Part2Solver.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    string line;
+    ifstream file("input\\input.txt");
+
+    int dialNum = 50;
+    int previousNum = 50;
+    int zeroCounter = 0;
+
+    int increment = 0;
+
+    while(getline(file, line))
+    {
+        previousNum = dialNum;
+        char direction = line[0];
+        int magnitude = stoi(line.substr(1));
+
+        if(direction == 'L')
+        {
+            increment = -1;
+        }
+        else
+        {
+            increment = 1;
+        }
+
+        for(int i = 0; i < magnitude; i++)
+        {
+            dialNum += increment;
+
+            if(dialNum > 99)
+            {
+                dialNum = 0;
+            }
+            else if(dialNum < 0)
+            {
+                dialNum = 99;
+            }
+
+            if(dialNum == 0)
+            {
+                zeroCounter++;
+            }
+        }
+
+        
+
+    }
+
+    file.close();
+
+    cout << zeroCounter;
+
+    return 0;
+}
+
+
