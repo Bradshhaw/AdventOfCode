aa89461bcbbe21beabbe361dd20056cfeabaa288
Alex Bradshaw <xander.bradshaw@gmail.com>
2025-12-03 13:12:07 +0800

    Day Two Part Two
    Added in the solver for part two, not performant at all but it gets the job done I guess.

--- Day 2/InvalidNumbersOne.cpp
+++ Day 2/InvalidNumbersOne.cpp
@@ -31,14 +31,51 @@ int powerOfTen(long long digits)
     return power;
 }
 
-int main()
+bool checkPattern(long long numIn)
 {
-    //Read in the file and tokenize based on the commas, split each token on the hyphen and then you can increment between the two numbers.
-    //Split the number as a string and then check for equality.
-    //Increment through and see if the first half of a string equals the second half of a string.
-    //If it does then grab the number it is, if not then move to next in the range.
-    //If the length of the string is not divisible by 2, then it doesn't have an ID.
+    string previousSection;
+    string thisSection;
 
+    for(int i = 2; i <= numDigits(numIn); i++)
+    {
+        if(numDigits(numIn) % i == 0)
+        {
+            string stringNum = to_string(numIn);
+            int numSame = 1;
+
+            for(int j = 0; j < i; j++)
+            {
+                if(j == 0)
+                {
+                    previousSection = stringNum.substr(0, numDigits(numIn)/i);
+                    stringNum.erase(0, numDigits(numIn)/i);
+                }
+                else 
+                {
+                    thisSection = stringNum.substr(0, numDigits(numIn)/i);
+                    stringNum.erase(0, numDigits(numIn)/i);
+
+                    if(previousSection == thisSection)
+                    {
+                        numSame++;
+                    }
+                    
+                    previousSection = thisSection;
+                }
+            }
+
+            if(numSame == i)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false; 
+}
+
+void PartOne()
+{
     ifstream t("input\\input.txt");
     stringstream buffer;
     buffer << t.rdbuf();
@@ -60,11 +97,9 @@ int main()
         firstNum = stoll(splitNumbers[0]);
         secondNum = stoll(splitNumbers[1]);
 
-        //cout << firstNum << "-" << secondNum << "\n";
 
         for(ii = firstNum; ii <= secondNum; ii++)
         {
-            //cout << ii << "\n";
             if(numDigits(ii) % 2 == 0)
             {
                 firstHalf = ii / (powerOfTen(numDigits(ii)/2));
@@ -73,14 +108,61 @@ int main()
                 if(firstHalf == secondHalf)
                 {
                     total += ii;
-                    cout << ii << "\n";
+                    //cout << ii << "\n";
                 }
             }
         }
 
     }
 
-    cout << "\n\n" << total << "\n";
+    cout << total;
+}
+
+void PartTwo()
+{
+    ifstream t("input\\input.txt");
+    stringstream buffer;
+    buffer << t.rdbuf();
+
+
+    vector<string> dataIn = split(buffer.str(), " ,");
+    long long total = 0;
+
+    long long firstNum;
+    long long secondNum;
+    
+    long long ii;
+
+    for(int i = 0; i < dataIn.size(); i++)
+    {
+        vector<string> splitNumbers = split(dataIn[i], "-");
+
+        firstNum = stoll(splitNumbers[0]);
+        secondNum = stoll(splitNumbers[1]);
+
+        for(ii = firstNum; ii <= secondNum; ii++)
+        {
+            if(checkPattern(ii))
+            {
+                total += ii;
+                //cout << ii << "\n";
+            }
+        }
+
+    }
+
+    cout << total;
+}
+
+int main()
+{
+    //Read in the file and tokenize based on the commas, split each token on the hyphen and then you can increment between the two numbers.
+    //Split the number as a string and then check for equality.
+    //Increment through and see if the first half of a string equals the second half of a string.
+    //If it does then grab the number it is, if not then move to next in the range.
+    //If the length of the string is not divisible by 2, then it doesn't have an ID.
+
+    PartTwo();
 
     return 0;
 }
